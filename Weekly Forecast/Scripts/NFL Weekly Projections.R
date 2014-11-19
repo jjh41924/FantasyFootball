f620ca99c655da99eb3b096562aee285511e83fa
jjh41924 <jhamiltonquanttrader@gmail.com>
2014-11-18 20:55:32 -0600

    FOX projections are specific for current week

--- Weekly Forecast/Scripts/NFL Weekly Projections.R
+++ Weekly Forecast/Scripts/NFL Weekly Projections.R
@@ -132,7 +132,7 @@ setPointsForWeek_nfl = function(week){
 }
 
 getNFL_Projections = function(week){
-  return(getProjections(paste(getMYFFDir(),"/Weekly Forecast/ESPN/Projections_Week_",week,"_Date_",sep="")));
+  return(getProjections(paste(getMYFFDir(),"/Weekly Forecast/NFL/Projections_Week_",week,"_Date_",sep="")));
   #write.csv(file=paste("C:/MY_FF/Weekly Forecast/ESPN/Projections_Week_",week,"_Date_",strftime(Sys.time(), format = "%Y_%m_%d"),".csv", sep=""), row.names=FALSE)
 }
 
