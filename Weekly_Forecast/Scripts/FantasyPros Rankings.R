298c23ee2bf6f67a588e1dbbc84190f485c3a0d6
FantasyFootballEngine <ff@localhost.localdomain>
2015-10-27 19:28:10 -0700

    Removed spaces from Fantasy Pros Rankings

--- Weekly_Forecast/Scripts/FantasyPros Rankings.R
+++ Weekly_Forecast/Scripts/FantasyPros Rankings.R
@@ -30,45 +30,45 @@ FF.FantasyPros.Parse.Table = function(p) {
 
 FF.FantasyPros.Set.Kicker = function(week) {
   num = FF.FantasyPros.Parse.Kicker.Table(readHTMLTable("http://www.fantasypros.com/nfl/rankings/k.php", stringsAsFactors = FALSE)$data)
-  save(num, file = paste(getMYFFDir(),"/Weekly_Forecast/FantasyPros Rankings/Kicker_Rankings_Week_",week,"_Date_",strftime(Sys.time(), format = "%Y_%m_%d"),".RData", sep=""))
-  write.csv(num, file=paste(getMYFFDir(),"/Weekly_Forecast/FantasyPros Rankings/Kicker_Rankings_Week_",week,"_Date_",strftime(Sys.time(), format = "%Y_%m_%d"),".csv", sep=""), row.names=FALSE)
+  save(num, file = paste(getMYFFDir(),"/Weekly_Forecast/FantasyPros_Rankings/Kicker_Rankings_Week_",week,"_Date_",strftime(Sys.time(), format = "%Y_%m_%d"),".RData", sep=""))
+  write.csv(num, file=paste(getMYFFDir(),"/Weekly_Forecast/FantasyPros_Rankings/Kicker_Rankings_Week_",week,"_Date_",strftime(Sys.time(), format = "%Y_%m_%d"),".csv", sep=""), row.names=FALSE)
   cat(paste("[FantasyPros K] DONE. week[",week,"]\n",sep=""))
 }
 
 FF.FantasyPros.Set.DST = function(week) {
   num = FF.FantasyPros.Parse.DST.Table(readHTMLTable("http://www.fantasypros.com/nfl/rankings/dst.php", stringsAsFactors = FALSE)$data)
-  save(num, file = paste(getMYFFDir(),"/Weekly_Forecast/FantasyPros Rankings/DST_Rankings_Week_",week,"_Date_",strftime(Sys.time(), format = "%Y_%m_%d"),".RData", sep=""))
-  write.csv(num, file=paste(getMYFFDir(),"/Weekly_Forecast/FantasyPros Rankings/DST_Rankings_Week_",week,"_Date_",strftime(Sys.time(), format = "%Y_%m_%d"),".csv", sep=""), row.names=FALSE)
+  save(num, file = paste(getMYFFDir(),"/Weekly_Forecast/FantasyPros_Rankings/DST_Rankings_Week_",week,"_Date_",strftime(Sys.time(), format = "%Y_%m_%d"),".RData", sep=""))
+  write.csv(num, file=paste(getMYFFDir(),"/Weekly_Forecast/FantasyPros_Rankings/DST_Rankings_Week_",week,"_Date_",strftime(Sys.time(), format = "%Y_%m_%d"),".csv", sep=""), row.names=FALSE)
   cat(paste("[FantasyPros DST] DONE. week[",week,"]\n",sep=""))
 }
 
 FF.FantasyPros.Set.ppr.rest.of.season.rankings = function(week) {
   num = FF.FantasyPros.Parse.Table(readHTMLTable("http://www.fantasypros.com/nfl/rankings/ros-ppr-qb-flex.php", stringsAsFactors = FALSE)$data)
-  save(num, file = paste(getMYFFDir(),"/Weekly_Forecast/FantasyPros Rankings/PPR_Rankings_Week_",week,"_Date_",strftime(Sys.time(), format = "%Y_%m_%d"),".RData", sep=""))
-  write.csv(num, file=paste(getMYFFDir(),"/Weekly_Forecast/FantasyPros Rankings/PPR_Rankings_Week_",week,"_Date_",strftime(Sys.time(), format = "%Y_%m_%d"),".csv", sep=""), row.names=FALSE)
+  save(num, file = paste(getMYFFDir(),"/Weekly_Forecast/FantasyPros_Rankings/PPR_Rankings_Week_",week,"_Date_",strftime(Sys.time(), format = "%Y_%m_%d"),".RData", sep=""))
+  write.csv(num, file=paste(getMYFFDir(),"/Weekly_Forecast/FantasyPros_Rankings/PPR_Rankings_Week_",week,"_Date_",strftime(Sys.time(), format = "%Y_%m_%d"),".csv", sep=""), row.names=FALSE)
   cat(paste("[FantasyPros PPR] DONE. week[",week,"]\n",sep=""))
 }
 FF.FantasyPros.Set.half.ppr.rest.of.season.rankings = function(week) {
   num = FF.FantasyPros.Parse.Table(readHTMLTable("http://www.fantasypros.com/nfl/rankings/ros-half-point-ppr-qb-flex.php", stringsAsFactors = FALSE)$data)
-  save(num, file = paste(getMYFFDir(),"/Weekly_Forecast/FantasyPros Rankings/Half_PPR_Rankings_Week_",week,"_Date_",strftime(Sys.time(), format = "%Y_%m_%d"),".RData", sep=""))
-  write.csv(num, file=paste(getMYFFDir(),"/Weekly_Forecast/FantasyPros Rankings/Half_PPR_Rankings_Week_",week,"_Date_",strftime(Sys.time(), format = "%Y_%m_%d"),".csv", sep=""), row.names=FALSE)
+  save(num, file = paste(getMYFFDir(),"/Weekly_Forecast/FantasyPros_Rankings/Half_PPR_Rankings_Week_",week,"_Date_",strftime(Sys.time(), format = "%Y_%m_%d"),".RData", sep=""))
+  write.csv(num, file=paste(getMYFFDir(),"/Weekly_Forecast/FantasyPros_Rankings/Half_PPR_Rankings_Week_",week,"_Date_",strftime(Sys.time(), format = "%Y_%m_%d"),".csv", sep=""), row.names=FALSE)
   cat(paste("[FantasyPros Half_PPR] DONE. week[",week,"]\n",sep=""))
 }
 
 
 getFantasyPros_PPR_ROS_Rankings = function(week){
   FF.FantasyPros.Set.ppr.rest.of.season.rankings(week)
-  return(getProjections(paste(getMYFFDir(),"/Weekly_Forecast/FantasyPros Rankings/PPR_Rankings_Week_",week,"_Date_",sep="")))
+  return(getProjections(paste(getMYFFDir(),"/Weekly_Forecast/FantasyPros_Rankings/PPR_Rankings_Week_",week,"_Date_",sep="")))
 }
 getFantasyPros_Half_PPR_ROS_Rankings = function(week){
   FF.FantasyPros.Set.half.ppr.rest.of.season.rankings(week)
-  return(getProjections(paste(getMYFFDir(),"/Weekly_Forecast/FantasyPros Rankings/Half_PPR_Rankings_Week_",week,"_Date_",sep="")))
+  return(getProjections(paste(getMYFFDir(),"/Weekly_Forecast/FantasyPros_Rankings/Half_PPR_Rankings_Week_",week,"_Date_",sep="")))
 }
 getFantasyPros_Kicker_Rankings = function(week){
   FF.FantasyPros.Set.Kicker(week)
-  return(getProjections(paste(getMYFFDir(),"/Weekly_Forecast/FantasyPros Rankings/Kicker_Rankings_Week_",week,"_Date_",sep="")))
+  return(getProjections(paste(getMYFFDir(),"/Weekly_Forecast/FantasyPros_Rankings/Kicker_Rankings_Week_",week,"_Date_",sep="")))
 }
 getFantasyPros_DST_Rankings = function(week){
   FF.FantasyPros.Set.DST(week)
-  return(getProjections(paste(getMYFFDir(),"/Weekly_Forecast/FantasyPros Rankings/DST_Rankings_Week_",week,"_Date_",sep="")))
+  return(getProjections(paste(getMYFFDir(),"/Weekly_Forecast/FantasyPros_Rankings/DST_Rankings_Week_",week,"_Date_",sep="")))
 }
